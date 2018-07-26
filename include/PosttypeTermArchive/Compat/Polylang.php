81975dab766d5d9619398ddcd75f9085fb49791c
mcguffin <joern@flyingletters.com>
2018-07-26 16:15:40 +0200

    fix pll alternate url for translated terms with no posts

--- include/PosttypeTermArchive/Compat/Polylang.php
+++ include/PosttypeTermArchive/Compat/Polylang.php
@@ -24,14 +24,27 @@ class Polylang extends Core\PluginComponent {
 	 *	@filter pll_translation_url
 	 */
 	public function translation_url( $url , $language_slug ) {
-		if ( is_post_type_archive() && (is_category() || is_tag() || is_tax() ) ) {
+
+		if ( ! is_paged() && is_post_type_archive() && (is_category() || is_tag() || is_tax() ) ) {
 			$term = get_queried_object();
 			$translated_term_id = pll_get_term( $term->term_id , $language_slug );
 			if ( $translated_term_id ) {
 				$post_type = get_post_type();
-				$url = get_post_type_term_link( $post_type , $translated_term_id , $term->taxonomy );
-				if ( is_wp_error( $url ) )
+				$url = get_post_type_term_link( $post_type , $translated_term_id, $term->taxonomy );
+				if ( is_wp_error( $url ) ) {
 					return false;
+				}
+				// if term has posts
+				global $wpdb;
+				$sql = "SELECT count(id) FROM $wpdb->posts AS p
+						LEFT JOIN $wpdb->term_relationships AS tr ON p.ID = tr.object_id
+						WHERE tr.term_taxonomy_id =%d
+						AND p.post_type=%s";
+				$res = $wpdb->get_var( $wpdb->prepare( $sql, $translated_term_id, $post_type ) );
+
+				if ( ! $res ) {
+					return false;
+				}
 			} else {
 				return false;
 			}
