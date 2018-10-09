567a11190a3fe4f849d5b13c2c64c46222f3a97f
mcguffin <joern@flyingletters.com>
2018-10-09 13:57:24 +0200

    Release version 0.3.8

--- include/PosttypeTermArchive/Core/Archive.php
+++ include/PosttypeTermArchive/Core/Archive.php
@@ -11,6 +11,16 @@ class Archive extends Singleton {
 
 
 	/**
+	 *	@var string	The post type
+	 */
+	private $current_archive_page = null;
+
+	/**
+	 *	@var string	The post type
+	 */
+	private $prev_post = null;
+
+	/**
 	 *	@inheritdoc
 	 */
 	protected function __construct( ) {
@@ -22,6 +32,11 @@ class Archive extends Singleton {
 		add_filter( 'get_the_archive_title', array( $this, 'archive_title' ) );
 		add_filter( 'get_the_archive_description', array( $this, 'archive_description' ) );
 
+		add_action( 'get_header', array( $this, 'get_current_post_type_archive_page' ) );
+		add_action( 'wp_head', array( $this, 'do_header' ), 0xffffffff );
+		add_action( 'done_header', array( $this, 'done_header' ) );
+
+
 		// add filter: post type archive url > page url
 //		add
 		$args = func_get_args();
@@ -29,13 +44,42 @@ class Archive extends Singleton {
 
 	}
 
+
+	/**
+	 *	@action wp_head
+	 */
+	public function do_header() {
+		// setup post data
+		if ( $page_id = $this->get_current_post_type_archive_page() ) {
+			if ( ! has_action('done_header')) {
+				return;
+			}
+
+			global $post;
+			if ( is_null( $this->prev_post ) ) {
+				$this->prev_post = $post;
+			}
+			$post = get_post( $page_id );
+
+			setup_postdata( $post );
+		}
+	}
+	/**
+	 *	@action done_header
+	 */
+	public function done_header() {
+		global $post;
+		setup_postdata( $this->prev_post );
+		$post = $this->prev_post;
+	}
+
 	/**
 	 *	@filter get_the_archive_title
 	 */
 	public function archive_title( $title ) {
 
-		if ( $archive_page_id = $this->get_current_post_type_archive_page() ) {
-			$title = get_the_title( $archive_page_id );
+		if ( $page_id = $this->get_current_post_type_archive_page() ) {
+			$title = get_the_title( $page_id );
 		}
 		return $title;
 	}
@@ -44,8 +88,9 @@ class Archive extends Singleton {
 	 *	@filter get_the_archive_description
 	 */
 	public function archive_description( $description ) {
-		if ( $archive_page_id = $this->get_current_post_type_archive_page() ) {
-			$description = apply_filters( 'the_content', get_post( $archive_page_id )->post_content );
+
+		if ( $page_id = $this->get_current_post_type_archive_page() ) {
+			$description = apply_filters( 'the_content', get_post( $page_id )->post_content );
 		}
 		return $description;
 	}
@@ -79,8 +124,12 @@ class Archive extends Singleton {
 
 	/**
 	 *	@return bool|int
+	 *	@action get_header
 	 */
 	public function get_current_post_type_archive_page() {
+		if ( ! is_null( $this->current_archive_page ) ) {
+			return $this->current_archive_page;
+		}
 		if ( ! is_post_type_archive() ) {
 			return false;
 		}
@@ -90,7 +139,8 @@ class Archive extends Singleton {
 		if ( ! $post_type = get_post_type() ) {
 			return false;
 		}
-		return $this->get_archive_page_id( $post_type );
+		$this->current_archive_page = $this->get_archive_page_id( $post_type );
+		return $this->current_archive_page;
 
 	}
 
